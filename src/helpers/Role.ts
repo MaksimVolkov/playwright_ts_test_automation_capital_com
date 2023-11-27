0f4f030dbf64b9023252f32817e0d0526f8db75f
Maksim Volkau <volkau.maksim@gmail.com>
2023-11-27 16:07:54 +0200

    feat: add role: unregistered, authorized, unauthorized

--- src/helpers/Role.ts
+++ src/helpers/Role.ts
@@ -1,5 +1,8 @@
-import { Page } from '@playwright/test';
-
+import { expect, Page } from '@playwright/test';
+import { Selectors } from '../elements/Selectors';
+import 'dotenv/config';
+const { logIn } = Selectors.forms;
+const { buttonLogIn, buttonMyAccount, buttonLogout } = Selectors.header.buttons;
 export class Role {
   private page: Page;
 
@@ -29,18 +32,46 @@ export class Role {
 
   public async setupForAuthorizedUser(): Promise<void> {
     await this.page.goto('/'); // go to main page
-    // process.env.LOGIN
-    // process.env.PASSWORD
-    // TODO add steps
+    await this.page.waitForLoadState('load');
+    await this.page.locator(buttonLogIn).click();
+    await this.page.waitForSelector(logIn.logInBlock);
+
+    const modalIsVisible = this.page.locator(logIn.logInBlock);
+    if (modalIsVisible !== null) {
+      const title = await this.page.locator(logIn.titleH1).innerText(); // getting the title of the login form
+      expect(`${title}`).toBe('Login'); // checking the title
+      const emailField = this.page.locator(logIn.emailField); // getting the email field
+      const passwordField = this.page.locator(logIn.passwordField); // getting the password field
+
+      await emailField.fill(process.env.LOGIN!); // fill in the login input field
+      // expect(emailField.getAttribute('value')).toBe(process.env.LOGIN!); // checking the login input field
+
+      await passwordField.fill(process.env.PASSWORD!); // fill in the password entry field
+      // expect(passwordField.inputValue()).toBe(process.env.PASSWORD!); // checking the password entry field
+      // TODO add checkbox steps
+      // const checkbox = this.page.locator(logIn.checkbox); // checkbox - "Log me out after 7 days"
+
+      const submitButton = this.page.locator(logIn.submitButton); // getting the submit button
+      await submitButton.click();
+      await this.page.waitForURL(process.env.PLATFORM!);
+      await this.page.waitForLoadState('load');
+      await this.page.goto('/'); // go to main page
+      await this.page.waitForLoadState('load');
+    }
   }
 
   public async setupForUnauthorizedUser(): Promise<void> {
-    await this.page.goto('/'); // go to main page
-    // TODO add steps
+    await this.setupForAuthorizedUser();
+    const openMyAccount = this.page.locator(buttonMyAccount); // getting the submit button
+    await openMyAccount.click();
+    const logoutMyAccount = this.page.locator(buttonLogout); // getting the submit button
+    await logoutMyAccount.click({ timeout: 5000 });
+    await this.page.waitForLoadState('load');
   }
 
   public async setupForNotRegisteredUser(): Promise<void> {
     await this.page.context().clearCookies();
     await this.page.goto('/'); // go to main page
+    await this.page.waitForURL(process.env.MAIN_URL!);
   }
 }
