6743649237eecba123269d7475218fb9977844d4
Maksim Volkau <volkau.maksim@gmail.com>
2023-12-13 14:28:10 +0200

    refactor: selectors, form, testcases

--- src/elements/Form.ts
+++ src/elements/Form.ts
@@ -3,16 +3,18 @@ import { Forms } from './Selectors';
 
 export class Form {
   private readonly page: Page;
+  private readonly activeForm: typeof Forms.activeForm;
   private readonly formLogIn: typeof Forms.logIn.logInBlock;
   private readonly formSignUp: typeof Forms.signUp.signUpBlock;
 
   constructor(page: Page) {
     this.page = page;
+    this.activeForm = Forms.activeForm;
     this.formLogIn = Forms.logIn.logInBlock;
     this.formSignUp = Forms.signUp.signUpBlock;
   }
   async formIsVisible(): Promise<boolean> {
-    await this.page.waitForSelector(this.formSignUp);
+    await this.page.waitForSelector(this.activeForm);
     const signUpModal = this.page.locator(this.formSignUp);
     const logInModal = this.page.locator(this.formLogIn);
     const currentUrl = this.page.url();
@@ -20,17 +22,18 @@ export class Form {
     console.log(signUpModal + ' | ' + logInModal + ' | ' + currentUrl);
     console.log(await signUpModal.isVisible(), await logInModal.isVisible());
 
-    //
-    if ((await signUpModal.count()) === 1 || currentUrl.includes('/trading/signup')) {
-      console.log(`%c modal form SIGNUP is visible %c`, 'color: yellow; font-weight: bold;', 'color: inherit;');
-      return true;
-    } else if ((await logInModal.count()) === 1 || currentUrl.includes('/trading/login')) {
-      console.log('%c modal form LOGIN is visible %c', 'color: yellow; font-weight: bold;', 'color: inherit;');
-      return true;
+    const isSignUpVisible = (await signUpModal.count()) === 1 || currentUrl.includes('/trading/signup');
+    const isLogInVisible = (await logInModal.count()) === 1 || currentUrl.includes('/trading/login');
+
+    if (isSignUpVisible) {
+      console.log(`%c modal form SIGNUP is visible %c`, 'color: green; font-weight: bold;', 'color: inherit;');
+    } else if (isLogInVisible) {
+      console.log('%c modal form LOGIN is visible %c', 'color: green; font-weight: bold;', 'color: inherit;');
     } else {
       console.log('%c modal forms is not visible %c', 'color: orange; font-weight: bold;', 'color: inherit;');
-      return false;
     }
+
+    return isSignUpVisible || isLogInVisible;
   }
   public async isVisibleSignUp(): Promise<void> {
     // TODO add
